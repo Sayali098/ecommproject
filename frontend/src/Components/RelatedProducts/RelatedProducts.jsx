3ddc61835c8da0320d95e4b5b4fa13d98f7326cc
Sayali098 <shelakesayali9022@gmail.com>
2024-06-24 17:34:05 +0530

    added dataproduct in file

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import './relatedProducts.css'
-// import data_product from '../Assets/Data';
-import Item from '../Item/Item';
+import React from "react";
+import "./relatedProducts.css";
+import data_product from '../Assetss/Assets/data';
+import Item from "../Item/Item";
 
 const RelatedProducts = () => {
-    return (
-        <div className='relatedProducts'>
+  return (
+    <div className="relatedProducts">
+      <h1>Related Products</h1>
+      <hr></hr>
+      <div className="relatedproduct-item">
+        {data_product.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            ></Item>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-            <h1>Related Products</h1>
-            <hr></hr>
-            <div className='relatedproduct-item'>
-             {
-                data_product.map((item,i)=>{
-                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} ></Item>
-                })
-             }
-            </div>
-        </div>
-    )
-}
-
-export default RelatedProducts
+export default RelatedProducts;
